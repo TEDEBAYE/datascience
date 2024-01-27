e13b1fba75a38364c6e88554c42b0b6c4529438b
TEDEBAYE DIEUDONNE <tedebayedieudonn75@gmail.com>
2024-01-27 12:32:39 +0100

    initial commit

---
+++ comp.py
@@ -0,0 +1,8 @@
+print("enter a number x")
+x= int(input())
+print("enter a number y")
+y= int(input())
+result1= x>y
+print("return result is " + str(result1))
+result2= x<y
+print("return result " + str(result2))
---
+++ index.py
@@ -0,0 +1,26 @@
+print("enter a number x")
+x= int(input())
+print("enter a number y")
+y= int(input())
+
+result1= x * y
+print("return result is "  + str(result1)) 
+
+result2= x + y
+print("return result is "  + str(result2))
+
+result3= x - y
+print("return result is "  + str(result3)) 
+
+result4= x / y
+print("return result is "  + str(result4)) 
+
+result5=(x+y)*70/100
+print("return result is "  + str(result5)) 
+
+result6= x % y
+print("return result is "  + str(result6)) 
+
+result7= x**y
+print("return result is "  + str(result7)) 
+
